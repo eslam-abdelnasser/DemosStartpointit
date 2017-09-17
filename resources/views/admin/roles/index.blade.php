7253ca62f3d660fc58b8d9ad4f24b5f6ac3868b5
Start_point <startpointwork@gmail.com>
2017-09-17 16:33:24 +0200

    MOdify error on create admin

---
+++ resources/views/admin/roles/index.blade.php
@@ -0,0 +1,40 @@
+@extends('admin.layout')
+
+@section('title', 'show languages')
+
+{{-- start css --}}
+@section('css')
+
+@endsection
+{{-- end css --}}
+
+{{-- Start Breadcums --}}
+
+@section('home','Dashboard')
+@section('page_title','Show Roles')
+
+
+{{-- End Breadcums--}}
+
+
+{{-- Start page title --}}
+
+@section('page_head','Languages')
+
+@section('page_description','Show all website languages')
+
+{{-- end page title --}}
+
+
+@section('content')
+
+
+@endsection
+
+
+{{-- Start javascript --}}
+@section('js')
+
+@endsection
+
+{{-- end javascript --}}
